a678f0bb8b7e9e8078d11a83edbbbb4fa279f1e3
sweetienianyu <475493955@qq.com>
2022-03-20 10:54:30 +0800

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import/prefer-default-export': 0,
+    'linebreak-style': 0,
+  },
+};
